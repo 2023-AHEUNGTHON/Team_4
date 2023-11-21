b6028b87ebd29832bc543cb20a640f6681f348d9
ckdwns1221 <ckdwns2181@gmail.com>
2023-11-21 11:58:30 +0900

    setting

---
+++ OneDrive/문서/바탕 화면/SoloStudy/StudyPython/CH02/type.py
@@ -0,0 +1,6 @@
+print(type("안녕하세요")) # <class 'str'>
+print(type('안녕하세요')) # <class 'str'>
+print(type(52)) # <class 'int'>
+print(type(52.273)) # <class 'float'>
+print(type(true)) # <class 'boolean'> 일거라 생각했지만 NameError 발생
+
